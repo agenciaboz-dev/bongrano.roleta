0f843d02ba26a64d7666519b3675f076c4c5e0f9
nandobfer <nandobfer@yahoo.com.br>
2023-10-04 17:04:18 -0300

    update: removed auto complete com textfield component

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -2,5 +2,5 @@ import React from "react"
 import { Box, TextField as MuiText, TextFieldProps } from "@mui/material"
 
 export const TextField: React.FC<TextFieldProps> = (props) => {
-    return <MuiText {...props} InputProps={{ ...props.InputProps, sx: { ...props.InputProps?.sx, bgcolor: "white" } }} />
+    return <MuiText {...props} InputProps={{ ...props.InputProps, sx: { ...props.InputProps?.sx, bgcolor: "white" } }} autoComplete="off" />
 }
