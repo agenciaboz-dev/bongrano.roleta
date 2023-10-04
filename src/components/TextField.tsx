09c2bb13eae2151a44acd4d899da25c799d86649
nandobfer <nandobfer@yahoo.com.br>
2023-10-04 17:10:35 -0300

    update: label bgcolor

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -2,5 +2,12 @@ import React from "react"
 import { Box, TextField as MuiText, TextFieldProps } from "@mui/material"
 
 export const TextField: React.FC<TextFieldProps> = (props) => {
-    return <MuiText {...props} InputProps={{ ...props.InputProps, sx: { ...props.InputProps?.sx, bgcolor: "white" } }} autoComplete="off" />
+    return (
+        <MuiText
+            {...props}
+            InputProps={{ ...props.InputProps, sx: { ...props.InputProps?.sx, bgcolor: "white" } }}
+            autoComplete="off"
+            InputLabelProps={{ sx: { bgcolor: "white", borderRadius: "2vw" } }}
+        />
+    )
 }
